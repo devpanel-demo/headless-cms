13dc13696ed49896f3a5a118965960a52f0253ce
Boi Nguyen <boi@devpanel.com>
2025-02-20 11:20:43 +0000

    CI - install nodejs16

--- .devpanel/custom_package_installer.sh
+++ .devpanel/custom_package_installer.sh
@@ -9,11 +9,14 @@
 #
 # ----------------------------------------------------------------------
 if [[ ! $(which npm) ]]; then
-    cd ~
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    source $HOME/.nvm/nvm.sh
-    nvm install 16
-    nvm use 16
-    source ~/.bashrc 
-fi
+    # cd ~
+    # curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+    # source $HOME/.nvm/nvm.sh
+    # nvm install 16
+    # nvm use 16
+    # source ~/.bashrc
 
+    cd ~
+    curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+    sudo apt -y install nodejs 
+fi
