9119207c27c27dd6d55e6bff385b92047a3a20ea
boi <boi@devpanel.com>
2025-02-21 03:25:07 +0000

    Update init-container

--- .devpanel/init-containter.sh
+++ .devpanel/init-containter.sh
@@ -14,8 +14,6 @@
 #
 # For GNU Affero General Public License see <https://www.gnu.org/licenses/>.
 # ----------------------------------------------------------------------
-
-cd $APP_ROOT/backend
 if [[ $(mysql -h$DB_HOST -P$DB_PORT -u$DB_USER -p$DB_PASSWORD $DB_NAME -e "show tables;") == '' ]]; then
   if [[ -f "$APP_ROOT/.devpanel/dumps/db.sql.tgz" ]]; then
     echo  'Extract mysql files ...'
